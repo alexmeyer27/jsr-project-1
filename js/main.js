0b21848cfd6a0092950ed9be08bb7ff82f84f6cc
Alex Meyer <alexmeyer27@gmail.com>
2018-08-06 18:28:06 -0600

    initial attempt at solving the problem

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
 // add an event listener to the form to submit Dave's message
 
+var myInput = document.getElementById("chatInput"),
+	daveChat = document.getElementById("dave"),
+	halChat = document.getElementById("hal"),
+	readInput = myInput.value;
+
+myInput.addEventListener( 'input', function () {
+	readInput
+});
+
+var displayDaveMessage = function () {
+		daveChat.innerHTML = readInput;
+	};
+
+if (readInput !== null) {
+	displayDaveMessage();
+};
+	
 // create a function for HAL to respond to Dave's messages with variable logic based upon
 // Dave's inputs
 
 // create a function for HAL to open the chat with "Good morning, Dave"
 
+
+
 // invoke the opening message
+var openingMessage = function () {
+	halChat.innerHTML = "Good morning, Dave";
+	};
+
+openingMessage();
