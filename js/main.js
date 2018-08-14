360ba320fd04b1496304f32aae1d057280f8928d
Alex Meyer <alexmeyer27@gmail.com>
2018-08-13 19:49:05 -0600

    weekly changes

--- js/main.js
+++ js/main.js
@@ -1,32 +1,41 @@
 // add an event listener to the form to submit Dave's message
 
-var myInput = document.getElementById("chatInput"),
-	daveChat = document.getElementById("dave"),
-	halChat = document.getElementById("hal"),
-	readInput = myInput.value;
+var chatForm = document.getElementById('chatForm'),
+	chatInput = document.getElementById('chatInput'),
+	daveChat = document.getElementById('dave'),
+	halChat = document.getElementById('hal');
 
-myInput.addEventListener( 'input', function () {
-	readInput
+var displayDaveMessage = function(daveMessage) {
+	daveChat.innerHTML = daveMessage;
+};
+	
+chatForm.addEventListener('submit', function(){
+	event.preventDefault();
+	displayDaveMessage(chatInput.value);
 });
 
-var displayDaveMessage = function () {
-		daveChat.innerHTML = readInput;
-	};
 
-if (readInput !== null) {
-	displayDaveMessage();
-};
+
 	
 // create a function for HAL to respond to Dave's messages with variable logic based upon
 // Dave's inputs
 
 // create a function for HAL to open the chat with "Good morning, Dave"
 
-
-
-// invoke the opening message
 var openingMessage = function () {
 	halChat.innerHTML = "Good morning, Dave";
 	};
 
+// invoke the opening message
+
 openingMessage();
+
+// chatInput.addEventListener('submit', function(){
+// 	event.preventDefault();
+// 	displayDaveMessage(chatInput.value);
+// });
+
+
+// if (myInput.value !== undefined) {
+// 	displayDaveMessage();
+// };
